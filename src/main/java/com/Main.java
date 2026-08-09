06e92984976b9e6077bbdc9874c3de5069a09a8b
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-08-09 11:32:20 +0545

    implemented abstract class (Abstraction)

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,17 +1,25 @@
 package com;
-
+import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        interface Rentable {
+            double calculateRentalCost(int days);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+            boolean isAvailableForRent();
+        }
+        abstract class Vehicle implements Rentable {
+            // ENCAPSULATION
+            private final String plateNumber;
+            private final String model;
+            private boolean rented;
+
+            public Vehicle(String plateNumber, String model) {
+                this.plateNumber = plateNumber;
+                this.model = model;
+                this.rented = false;
+            }
+
         }
     }
-}
